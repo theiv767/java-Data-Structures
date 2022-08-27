62202dae1a55b51112f195ea0c4cecbf9631752e
Davi Barbosa Vieira Bispo <davib.g767@gmail.com>
2022-08-26 21:19:38 -0300

    init VectorList

---
+++ static-storage-list/VectorList.java
@@ -0,0 +1,59 @@
+public class VectorList {
+    private Object[] elements;
+    private int cont = 0;
+
+    VectorList() {
+        elements = new Object[10];
+    }
+
+    VectorList(int n) {
+        elements = new Object[n];
+    }
+
+    public void add(int value) {
+        if (cont == this.elements.length - 1) incrementSize();
+
+        elements[cont] = value;
+        cont++;
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException, IllegalArgumentException {
+        if (cont <= 0) {
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+        if (index <= cont) throw new IndexOutOfBoundsException("index out of bounds");
+
+        cont--;
+        for (int i = index; i < cont; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+    }
+
+
+    public void clearAll() {
+        cont = 0;
+        this.elements = new Object[10];
+
+    }
+
+
+    //---------------------------
+    private void incrementSize() {
+        Object[] aux = new Object[cont + (cont / 2)];
+        for (int i = 0; i < cont; i++) {
+            aux[i] = this.elements[i];
+        }
+        this.elements = aux;
+    }
+
+    @Override
+    public String toString() {
+        String elementsString = "[";
+        for (int i = 0; i < cont; i++)
+            elementsString += (i == cont - 1) ? this.elements[i] : this.elements[i] + ", ";
+
+        elementsString += "]";
+        return elementsString;
+    }
+}
+
